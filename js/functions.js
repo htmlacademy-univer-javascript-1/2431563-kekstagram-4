2afc5884cb31e48395370411e8842a723084b759
Софья Сережкина <anatolygulkin@yandex.ru>
2023-10-01 01:11:19 +0500

    добавлены функции

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
-let checkLengthString = function(string, length) {
+const checkLengthString = function(string, length) {
   return string.length <= length;
-}
+};
 
 checkLengthString('проверяемая строка', 20);
 
@@ -9,26 +9,26 @@ checkLengthString('проверяемая строка', 20);
 мы могли бы и сразу в цикле символы сравнивать и прервать программу,
 когда попадется первое несовпадение, но ладно...*/
 
-let checkPalindrome = function(string) {
-  let originalString = string.replaceAll(' ', '').toLowerCase();
+const checkPalindrome = function(string) {
+  const originalString = string.replaceAll(' ', '').toLowerCase();
   let reversedString = '';
   for (let i = originalString.length - 1; i >= 0; i--) {
-      reversedString += originalString[i];
+    reversedString += originalString[i];
   }
   return originalString === reversedString;
-}
+};
 
 checkPalindrome('Лёша на полке клопа нашёл ');
 
-let findNumbers = function(str) {
-  let string = str.toString();
+const findNumbers = function(str) {
+  const string = str.toString();
   let number = '';
   for (let i = 0; i < string.length; i++) {
-    if (!Number.isNaN(parseInt(string[i]))) {
-        number += string[i];
+    if (!Number.isNaN(parseInt(string[i], 10))) {
+      number += string[i];
     }
   }
-  return parseInt(number);
-}
+  return parseInt(number, 10);
+};
 
 findNumbers(1.5);
