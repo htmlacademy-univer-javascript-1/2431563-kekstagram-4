fa5076974932839f5345e0d026bada47b172fc25
Keks <keks@htmlacademy.ru>
2023-10-23 10:51:38 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,34 @@
+const checkLengthString = function(string, length) {
+  return string.length <= length;
+};
+
+checkLengthString('проверяемая строка', 20);
+
+/* сделала так, как расписано в задании, но я не очень понимаю,
+почему нужно так усложнять себе жизнь, так как, если мы уже заморочились циклами,
+мы могли бы и сразу в цикле символы сравнивать и прервать программу,
+когда попадется первое несовпадение, но ладно...*/
+
+const checkPalindrome = function(string) {
+  const originalString = string.replaceAll(' ', '').toLowerCase();
+  let reversedString = '';
+  for (let i = originalString.length - 1; i >= 0; i--) {
+    reversedString += originalString[i];
+  }
+  return originalString === reversedString;
+};
+
+checkPalindrome('Лёша на полке клопа нашёл ');
+
+const findNumbers = function(str) {
+  const string = str.toString();
+  let number = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string[i], 10))) {
+      number += string[i];
+    }
+  }
+  return parseInt(number, 10);
+};
+
+findNumbers(1.5);
---
+++ 4/js/main.js
@@ -0,0 +1,2 @@
+import { getObjects } from './modules/createObjects';
+getObjects();
